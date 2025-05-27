849511d752b8c63ddd874162cec2e267cd1c912b
mathohub <mathonfer@gmail.com>
2025-05-27 14:26:20 -0300

    feat: ajustar estilo e componente de palavra secreta

--- SecretWord/src/App.jsx
+++ SecretWord/src/App.jsx
@@ -20,18 +20,48 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-const [pickedWord, setPickedWord] = useState("");
-const [pickedCategory, setPickedCategory] = useState("");
-const [letters, setLetters] = useState([]);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    // pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category }
+
+  };
+
+
 
 
   //começa o jogo
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
 
 
 
+    console.log(word, category)
+    console.log(wordLetters)
 
-  setGameStage(stages[1].name);
+    
+    setGameStage(stages[1].name);
   };
 
   const verifyLetter = () => {
@@ -39,18 +69,17 @@ const [letters, setLetters] = useState([]);
   }
 
   const retry = () => {
-     setGameStage(stages[0].name);
+    setGameStage(stages[0].name);
   }
 
-  console.log(words);
 
 
   return (
     <>
       <div className="App">
         {gameStage === "start" && <StartScreen startGame={startGame} />}
-        {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
-        {gameStage === "end" && <GameOver retry={retry}/>}
+        {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+        {gameStage === "end" && <GameOver retry={retry} />}
       </div>
 
     </>
--- SecretWord/src/components/Game.jsx
+++ SecretWord/src/components/Game.jsx
@@ -1,11 +1,41 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+// eslint-disable-next-line no-unused-vars
+const Game = ({ verifyLetter }) => {
   return (
-    <>
-    <div>Game</div>
-    <button onClick={verifyLetter}>Finalizar jogo</button>
-    </>
+
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"> </span>
+      </div>
+
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
+
+
+    </div>
+
   )
 }
 
